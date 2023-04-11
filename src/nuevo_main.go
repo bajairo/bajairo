207faed00de564af5a9d771b5ac0f6e5e9306b8f
bajairo <bajairocuellar@gmail.com>
2023-04-10 20:00:23 -0500

    informacion_de_nuevos_cambios

---
+++ src/nuevo_main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func normalFunction(message string) {
+	fmt.Println(message)
+}
+
+// cuando se va a invocar una funcion concatenada se le da la primera en minuscula
+//y la segunda en mayuscula ejemplo tripleArgument
+//es un abuena practica que cuando se va a nombrar mas de una variable y tiene el mismo nobre solo se marca la ultima
+// de las que son es decir que a y b son la misma variable ejemplo a, int b, int c, string se coloca a, b int, c string
+
+func tripleArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+// si necesitamos que la funcion retorne un valor
+
+func returnValue(a int) int {
+	return a * 2
+}
+
+func main() {
+	normalFunction("Hola mundo")
+	tripleArgument(1, 2, "Hola")
+
+	value := returnValue(2)
+	fmt.Println("value:", value)
+}
